25b7215f3e0f9f3843769a4f85baee05609bea4c
oracle <oracle@oel64.(none)>
2016-03-14 16:17:34 -0300

    Cambios en style de tabla

--- Scripts/test_umts.sh
+++ Scripts/test_umts.sh
@@ -3,3 +3,5 @@ FILE='/home/oracle/CYR/Datos/status_umts_gral_OSSRC3.html'
 /home/oracle/sqlcl/bin/sql -s harriague/HARRIAGUE@PERDIDO @/home/oracle/CYR/Scripts/status_umts_gral.sql 07.03.2016 07.03.2016 OSSRC3 22543 24915 $FILE
 sed -i 's/&lt;/</g' $FILE
 sed -i 's/&gt;/>/g' $FILE
+sed -i 's/14px/10px/g' $FILE
+sed -i 's/Georgia/Verdana/g' $FILE

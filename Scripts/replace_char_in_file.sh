5d7cf2e4a4def55b0589475d4f147835f51666eb
Marcelo Carrasco <marceloacarrasco@gmail.com>
2016-04-05 17:13:55 -0300

    modificaciones 05042016

--- Scripts/replace_char_in_file.sh
+++ Scripts/replace_char_in_file.sh
@@ -6,4 +6,4 @@ sed -i 's/14px/10px/g' $1
 sed -i 's/Georgia/Verdana/g' $1
 sed -i '/<script type=/,/script>/d' $1
 sed -i '/<div><input /d' $1
-
+cat $1 | tr "\n" "|" | grep -o '<table>.*</table>' | sed 's/\(<table>\|<\/table>\)//g' | sed 's/|/\n/g' > $2 
